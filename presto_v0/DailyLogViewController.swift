bc30a2563b0420879558ebd5c5ea058d767d335a
Ellen Sartorelli <esartorelli@middlebury.edu>
2017-04-26 11:01:00 -0400

    alert controller progress

---
+++ presto_v0/DailyLogViewController.swift
@@ -0,0 +1,60 @@
+//
+//  DailyLogViewController.swift
+//  presto_v0
+//
+//  Created by Ellen Sartorelli on 4/25/17.
+//  Copyright © 2017 Ellen Sartorelli. All rights reserved.
+//
+
+import UIKit
+
+class DailyLogViewController: UIViewController {
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    //MARK: - Action
+    
+    @IBAction func showAlert() {
+        let alertController = UIAlertController(title: "Select an item to add to your Daily Log", message: nil, preferredStyle: .actionSheet)
+    
+        
+        let taskAction = UIAlertAction(title: "Task", style: .default, handler: nil)
+        let eventAction = UIAlertAction(title: "Event", style: .default, handler: nil)
+        let reflectionAction = UIAlertAction(title: "Reflection", style: .default, handler: nil)
+
+        let defaultAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        
+        alertController.addAction(taskAction)
+        alertController.addAction(eventAction)
+        alertController.addAction(reflectionAction)
+        alertController.addAction(defaultAction)
+        
+        present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
